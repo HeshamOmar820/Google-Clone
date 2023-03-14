31c55809854990b95e23edccfaf7a790509f5ab5
heshamomar820 <heshamomar820@gmail.com>
2023-03-14 20:30:51 +0200

    updating api key

--- keys.js
+++ keys.js
@@ -1,7 +1,8 @@
 //export const API_KEY = 'AIzaSyAXgm7aW2thkA1lZaYiPmYH3Ve2Diay8pM';
 //export const API_KEY = 'AIzaSyAfXYk8ysH5SbcnNHWeiOqZFs3wT-PE2qA';
 //export const API_KEY = 'AIzaSyCTdUUJANvPkmbJPI6FJBtXLiyFrxZa3bo';
-export const API_KEY = 'AIzaSyAHEJH8uCVB3yKWrdWy7josk9YvI9Ux4uI';
+//export const API_KEY = 'AIzaSyAHEJH8uCVB3yKWrdWy7josk9YvI9Ux4uI';
+export const API_KEY = 'AIzaSyCnH2qRcAcaSuDuOsxbWUzddcX1ccwGlqQ';
 
 export const CONTEXT_KEY = 'f6d16b545e75a4198';
 //export const CONTEXT_KEY = '76a2d7a54db864abc';
